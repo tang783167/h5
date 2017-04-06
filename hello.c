0df83c97cbc6f4419a6d95520bc95ccd6694ddb5
Richard Wang <leix.wang@outlook.com>
2017-04-06 09:53:16 +0800

    New file css.
    add new file mycss.css.

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 
 
 int main(void){
-    printf("hello world!\n");
+    printf("hello world!\n"); 
+    printf("hello H5!\n");
+    printf("hello China!\n");
+    printf("hello BJ!\n");
+    printf("hello XA!\n");
     return 0;
 }
